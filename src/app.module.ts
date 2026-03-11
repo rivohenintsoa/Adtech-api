ee449e71b241b17079fe4ca6b7a303603ec45d65
rivohenintsoa <rivohenintsoaavotra@gmail.com>
2026-03-11 18:47:53 +0300

    feat: add statistics endpoint for campaigns

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CampaignsModule } from './campaigns/campaigns.module';
 import { AdsModule } from './ads/ads.module';
+import { StatsModule } from './stats/stats.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/adtech'),
     CampaignsModule,
-    AdsModule
+    AdsModule,
+    StatsModule
   ],
   controllers: [AppController],
   providers: [AppService],
