66f80a4b35ea3fdfa46c91b011ded4e6834c521f
rivohenintsoa <rivohenintsoaavotra@gmail.com>
2026-03-11 16:25:00 +0300

    feat: add campaigns listing with filters

--- src/campaigns/campaigns.service.ts
+++ src/campaigns/campaigns.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Campaign, CampaignDocument } from './schemas/campaign.schema';
 import { Model } from 'mongoose';
-import { CreateCampaignDto } from './dto/create-campaign.dto'
+import { CreateCampaignDto } from './dto/create-campaign.dto';
 
 @Injectable()
 export class CampaignsService {
@@ -17,5 +17,23 @@ export class CampaignsService {
       status: 'active',
     });
     return campaign.save();
+  }
+
+  async findAll(filters: any) {
+    const query: any = {};
+
+    if (filters.status) {
+      query.status = filters.status;
+    }
+
+    if (filters.advertiser) {
+      query.advertiser = filters.advertiser;
+    }
+
+    if (filters.country) {
+      query.targetCountries = filters.country;
+    }
+
+    return this.campaignModel.find(query);
   }
 }
