fdc4fdc19f71ada38cdbc51759f48c498e4c044a
rivohenintsoa <rivohenintsoaavotra@gmail.com>
2026-03-11 15:39:25 +0300

    feat: add campaigns module, controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CampaignsModule } from './campaigns/campaigns.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/adtech')
+    MongooseModule.forRoot('mongodb://localhost:27017/adtech'),
+    CampaignsModule
   ],
   controllers: [AppController],
   providers: [AppService],
